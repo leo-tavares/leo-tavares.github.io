ac86a7ac977c6f1c02a383a072bb96279f3c274a
leo-tavares <leonardotavaresdev@gmail.com>
2019-04-24 03:57:26 -0300

    criação da page Preparação automotiva

---
+++ src/pages/automotive/Automotive.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Content from '../../components/Automotive/Body';
+
+const Automotive = () => {
+    return(
+        <Container className="home">
+            <Content/>
+        </Container>
+    )
+}
+
+export default Automotive;

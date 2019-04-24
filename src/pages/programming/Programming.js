ed559ab88ed4d33da7b69e3155402d11d3c667f0
leo-tavares <leonardotavaresdev@gmail.com>
2019-04-24 01:36:59 -0300

    criação da page Programação

---
+++ src/pages/programming/Programming.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Content from '../../components/Programming/Body';
+import '../PgStyle.css'
+const Programming = () => {
+    return(
+        <Container className="home">
+            <Content/>
+        </Container>
+    )
+}
+
+export default Programming;

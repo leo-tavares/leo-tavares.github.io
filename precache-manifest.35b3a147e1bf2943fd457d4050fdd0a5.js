96a29ce483d2cb1cd9dbeed823651113fb4575c9
leo-tavares <leonardotavaresdev@gmail.com>
2019-04-24 01:45:35 -0300

    Updates

---
+++ precache-manifest.35b3a147e1bf2943fd457d4050fdd0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "408cf5797bdc30770d02",
+    "url": "/leo-tavares/leo-tavares.github.io/static/css/main.db4a5207.chunk.css"
+  },
+  {
+    "revision": "408cf5797bdc30770d02",
+    "url": "/leo-tavares/leo-tavares.github.io/static/js/main.d199949b.chunk.js"
+  },
+  {
+    "revision": "cee7d9fb46cb0c13683b",
+    "url": "/leo-tavares/leo-tavares.github.io/static/js/runtime~main.0f061e06.js"
+  },
+  {
+    "revision": "45ddc0f8e69763edac07",
+    "url": "/leo-tavares/leo-tavares.github.io/static/js/2.90e6ada7.chunk.js"
+  },
+  {
+    "revision": "6134e0e019593a0de53534f25fd54f35",
+    "url": "/leo-tavares/leo-tavares.github.io/static/media/profile.6134e0e0.jpeg"
+  },
+  {
+    "revision": "de86e832c9d6cb19e6807b0d342102c2",
+    "url": "/leo-tavares/leo-tavares.github.io/index.html"
+  }
+];

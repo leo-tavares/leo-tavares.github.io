c3a4de3e5ad09a7f56f98d170e71225eda9c73e0
leo-tavares <leonardotavaresdev@gmail.com>
2019-04-24 03:58:56 -0300

    refactor entry point

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+document.title= "LeoTavares"
+ReactDOM.render(<App/>, document.querySelector("#root"))
